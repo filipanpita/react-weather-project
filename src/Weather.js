7409546c67f2e206e8506e8d4f60961649fc922d
filipanpita <filipanpita@gmail.com>
2025-05-22 11:47:29 +0100

    api

--- src/Weather.js
+++ src/Weather.js
@@ -32,8 +32,8 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiKey = "8ca7dd4e61360b90fb66918853670e48";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
