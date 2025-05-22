3b60678c23af0e3897fcaf62234b23812832e197
filipanpita <filipanpita@gmail.com>
2025-05-22 11:50:59 +0100

    Add hosting URL

--- src/App.js
+++ src/App.js
@@ -9,17 +9,9 @@ export default function App() {
         <Weather defaultCity="New York" />
 
         <footer>
-          This project was coded by{" "}
+          This project was coded by Filipa Pita and is{" "}
           <a
-            href="https://www.delac.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Matt Delac
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/wecodeschool/react-weather-app"
+            href="https://github.com/filipanpita/react-weather-project"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -27,7 +19,7 @@ export default function App() {
           </a>{" "}
           and{" "}
           <a
-            href="https://suspicious-beaver-111c4d.netlify.com/"
+            href="https://shecodes-reactweatherappproject.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
