cb664d9402f9035b0b4447d411e0b50a03d66c58
filipanpita <filipanpita@gmail.com>
2025-05-22 11:24:50 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather React</div>;
+  return (
+    <div className="App">
+      <h1>Weather React</h1>
+      <footer>
+        This project was coded by Filipa Pita and is{" "}
+        <a
+          href="https://github.com/filipanpita/react-weather-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
